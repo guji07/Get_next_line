00cd747ec7ef9c7d07547585aaf6080e719e9b10
guji07 <vova-buchenkov@yandex.ru>
2019-09-24 13:53:26 +0300

    123

---
+++ cmake-build-debug/CMakeFiles/get_next_line.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/get_next_line.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/get_next_line.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/libft/ft_bzero.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/libft/ft_bzero.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/libft/ft_memalloc.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/libft/ft_memalloc.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/libft/ft_memcpy.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/libft/ft_memcpy.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/libft/ft_memmove.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/libft/ft_memmove.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/libft/ft_strlen.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/libft/ft_strlen.c.obj"
+  "C:/Users/Public/Documents/ClionProjects/get_next_line/tests.c" "C:/Users/Public/Documents/ClionProjects/get_next_line/cmake-build-debug/CMakeFiles/get_next_line.dir/tests.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/get_next_line.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/get_next_line.dir/get_next_line.c.obj"
+  "CMakeFiles/get_next_line.dir/tests.c.obj"
+  "CMakeFiles/get_next_line.dir/libft/ft_strlen.c.obj"
+  "CMakeFiles/get_next_line.dir/libft/ft_memmove.c.obj"
+  "CMakeFiles/get_next_line.dir/libft/ft_bzero.c.obj"
+  "CMakeFiles/get_next_line.dir/libft/ft_memcpy.c.obj"
+  "CMakeFiles/get_next_line.dir/libft/ft_memalloc.c.obj"
+  "get_next_line.pdb"
+  "get_next_line.exe"
+  "get_next_line.exe.manifest"
+  "libget_next_line.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/get_next_line.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
